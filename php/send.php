593401528d84193d98fef1038f567fad9e978ebb
tomasPalavecino <tpalavecino@alumno.unlam.edu.ar>
2022-02-21 22:32:57 -0300

    php

---
+++ php/send.php
@@ -0,0 +1,16 @@
+<?php
+    $destinatario = 'tomypalavecino17@gmail.com';
+
+    $name = $_POST['name'];
+    $asunto = "Mensaje de Portafolio";
+    $email = $_POST['email'];
+    $msj = $_POST['msj'];
+
+
+    $mensajeCompleto= $msj . "\nAtentamente: " . $name;
+
+    mail($destinatario, $asunto, $email, $mensajeCompleto);
+    echo "<script>alert('Correo enviado exitosamente')</script>";
+    echo "<script> setTimeout(\"location.href='index.html'\",1000)</script>";
+
+?>
