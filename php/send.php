b734b4d08a962f052bee7cafc0e8978957255448
tomasPalavecino <tpalavecino@alumno.unlam.edu.ar>
2022-02-21 22:56:52 -0300

    php test

--- php/send.php
+++ php/send.php
@@ -1,16 +1,27 @@
 <?php
-    $destinatario = 'tomypalavecino17@gmail.com';
 
-    $name = $_POST['name'];
-    $asunto = "Mensaje de Portafolio";
-    $email = $_POST['email'];
-    $msj = $_POST['msj'];
+    if(isset($_POST['send'])){
+        if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['msj'])){
+            $destinatario = 'tomypalavecino17@gmail.com';
+
+            $name = $_POST['name'];
+            $asunto = "Mensaje de Portafolio";
+            $email = $_POST['email'];
+            $msj = $_POST['msj'];
 
 
-    $mensajeCompleto= $msj . "\nAtentamente: " . $name;
+            $mensajeCompleto= $msj . "\nAtentamente: " . $name;
 
-    mail($destinatario, $asunto, $email, $mensajeCompleto);
-    echo "<script>alert('Correo enviado exitosamente')</script>";
-    echo "<script> setTimeout(\"location.href='./index.html'\",1000)</script>";
+            $mail = mail($destinatario, $asunto, $email, $mensajeCompleto);
+            
+
+            if($mail){
+                echo "<script>alert('Correo enviado exitosamente')</script>";
+                echo "<script> setTimeout(\"location.href='./index.html'\",1000)</script>";
+            }
+
+
+        }
+    }
 
 ?>
